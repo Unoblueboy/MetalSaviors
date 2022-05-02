d8f096b8f3ffb888067017af8aef05cd54a13d53
Nathan Douglas <Nathan20467@gmail.com>
2022-05-02 13:14:45 +0100

    initialize git repositoty

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const METALSAVIORS = {};
+
+// Define constants here, such as:
+METALSAVIORS.foobar = {
+  'bas': 'METALSAVIORS.bas',
+  'bar': 'METALSAVIORS.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ METALSAVIORS.abilities = {
+  "str": "METALSAVIORS.AbilityStr",
+  "dex": "METALSAVIORS.AbilityDex",
+  "con": "METALSAVIORS.AbilityCon",
+  "int": "METALSAVIORS.AbilityInt",
+  "wis": "METALSAVIORS.AbilityWis",
+  "cha": "METALSAVIORS.AbilityCha"
+};
+
+METALSAVIORS.abilityAbbreviations = {
+  "str": "METALSAVIORS.AbilityStrAbbr",
+  "dex": "METALSAVIORS.AbilityDexAbbr",
+  "con": "METALSAVIORS.AbilityConAbbr",
+  "int": "METALSAVIORS.AbilityIntAbbr",
+  "wis": "METALSAVIORS.AbilityWisAbbr",
+  "cha": "METALSAVIORS.AbilityChaAbbr"
+};
