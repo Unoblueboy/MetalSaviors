8012b5c63a0817c92ef8753dd47caa5e6892a075
Nathan Douglas <Nathan20467@gmail.com>
2022-05-26 22:32:33 +0100

    added proper editing for cav unit skills

--- module/sheets/cav-sheet.mjs
+++ module/sheets/cav-sheet.mjs
@@ -30,4 +30,67 @@ export class MetalSaviorsCavSheet extends ItemSheet {
 
 		return context;
 	}
+
+	/** @override */
+	activateListeners(html) {
+		html.find(".cav-skill-name input").change((ev) => {
+			const newName = ev.target.value;
+			const prevName = $(ev.target)
+				.parents(".cav-skill-name")
+				.data("prevName");
+
+			if (newName === prevName) {
+				return;
+			}
+			const bonus = this.item.data.data.cavUnitPiloting[prevName];
+			this.item.update({
+				[`data.cavUnitPiloting.${newName}`]: bonus,
+				[`data.cavUnitPiloting.-=${prevName}`]: null,
+			});
+		});
+
+		html.find(".add-cav-unit-skill").click((ev) => {
+			const curTarget = $(ev.target).parents(".add-cav-unit-skill");
+			const newName = $(curTarget)
+				.siblings(".add-cav-unit-skill-name")
+				.children("input")
+				.first()
+				.val();
+			const newValue = $(curTarget)
+				.siblings(".add-cav-unit-skill-value")
+				.children("input")
+				.first()
+				.val();
+
+			if (!newName || !newValue) {
+				return;
+			}
+
+			const cavUnitPilotingSkills = Object.keys(
+				this.item.data.data.cavUnitPiloting
+			);
+			if (cavUnitPilotingSkills.includes(newName)) {
+				return;
+			}
+
+			const bonus = Number.parseInt(newValue);
+			if (!bonus) {
+				return;
+			}
+
+			this.item.update({
+				[`data.cavUnitPiloting.${newName}`]: bonus,
+			});
+		});
+		html.find(".delete-cav-unit-skill").click((ev) => {
+			const curTarget = $(ev.target).parents(".delete-cav-unit-skill");
+			const cavSkillName = curTarget
+				.siblings(".cav-skill-name")
+				.children("input")
+				.val();
+			this.item.update({
+				[`data.cavUnitPiloting.-=${cavSkillName}`]: null,
+			});
+		});
+	}
 }
