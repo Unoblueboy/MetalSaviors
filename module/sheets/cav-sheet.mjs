4d8f117eded371e5fe6db0cd07aa9b4c9c3d6487
Nathan Douglas <Nathan20467@gmail.com>
2022-06-02 20:09:52 +0100

    Added cav traits

--- module/sheets/cav-sheet.mjs
+++ module/sheets/cav-sheet.mjs
@@ -33,11 +33,11 @@ export class MetalSaviorsCavSheet extends ItemSheet {
 
 	/** @override */
 	activateListeners(html) {
+		super.activateListeners(html);
+
 		html.find(".cav-skill-name input").change((ev) => {
 			const newName = ev.target.value;
-			const prevName = $(ev.target)
-				.parents(".cav-skill-name")
-				.data("prevName");
+			const prevName = $(ev.target).parents(".cav-skill-name").data("prevName");
 
 			if (newName === prevName) {
 				return;
@@ -48,27 +48,16 @@ export class MetalSaviorsCavSheet extends ItemSheet {
 				[`data.cavUnitPiloting.-=${prevName}`]: null,
 			});
 		});
-
 		html.find(".add-cav-unit-skill").click((ev) => {
 			const curTarget = $(ev.target).parents(".add-cav-unit-skill");
-			const newName = $(curTarget)
-				.siblings(".add-cav-unit-skill-name")
-				.children("input")
-				.first()
-				.val();
-			const newValue = $(curTarget)
-				.siblings(".add-cav-unit-skill-value")
-				.children("input")
-				.first()
-				.val();
+			const newName = $(curTarget).siblings(".add-cav-unit-skill-name").children("input").first().val();
+			const newValue = $(curTarget).siblings(".add-cav-unit-skill-value").children("input").first().val();
 
 			if (!newName || !newValue) {
 				return;
 			}
 
-			const cavUnitPilotingSkills = Object.keys(
-				this.item.data.data.cavUnitPiloting
-			);
+			const cavUnitPilotingSkills = Object.keys(this.item.data.data.cavUnitPiloting);
 			if (cavUnitPilotingSkills.includes(newName)) {
 				return;
 			}
@@ -84,12 +73,53 @@ export class MetalSaviorsCavSheet extends ItemSheet {
 		});
 		html.find(".delete-cav-unit-skill").click((ev) => {
 			const curTarget = $(ev.target).parents(".delete-cav-unit-skill");
-			const cavSkillName = curTarget
-				.siblings(".cav-skill-name")
-				.children("input")
-				.val();
+			const cavSkillName = curTarget.siblings(".cav-skill-name").children("input").val();
 			this.item.update({
 				[`data.cavUnitPiloting.-=${cavSkillName}`]: null,
+			});
+		});
+
+		html.find(".cav-trait-name input").change((ev) => {
+			const newName = ev.target.value;
+			const prevName = $(ev.target).parents(".cav-trait-name").data("prevName");
+
+			if (newName === prevName) {
+				return;
+			}
+			const bonus = this.item.data.data.traits[prevName];
+			this.item.update({
+				[`data.traits.${newName}`]: bonus,
+				[`data.traits.-=${prevName}`]: null,
+			});
+		});
+		html.find(".add-cav-trait").click((ev) => {
+			const curTarget = $(ev.target).parents(".add-cav-trait");
+			const newName = $(curTarget).siblings(".add-cav-trait-name").children("input").first().val();
+			const newValue = $(curTarget).siblings(".add-cav-trait-value").children("textarea").first().val();
+
+			if (!newName || !newValue) {
+				return;
+			}
+
+			const traits = Object.keys(this.item.data.data.traits);
+			if (traits.includes(newName)) {
+				return;
+			}
+
+			const traitDescription = newValue;
+			if (!traitDescription) {
+				return;
+			}
+
+			this.item.update({
+				[`data.traits.${newName}`]: traitDescription,
+			});
+		});
+		html.find(".delete-cav-trait").click((ev) => {
+			const curTarget = $(ev.target).parents(".delete-cav-trait");
+			const traitName = curTarget.siblings(".cav-trait-name").children("input").val();
+			this.item.update({
+				[`data.traits.-=${traitName}`]: null,
 			});
 		});
 	}
