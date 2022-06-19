3c9d1f010dde993253272f43b96cd7a9b8a31a09
Nathan Douglas <Nathan20467@gmail.com>
2022-06-19 17:42:10 +0100

    #minor updated automatic tag generation to put tag into system manifest

--- .github/workflows/update_system_manifest.py
+++ .github/workflows/update_system_manifest.py
@@ -1,17 +1,22 @@
 import sys
 import json
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 4:
     raise Exception(f"Two arguments expected but {len(sys.argv)} given: {sys.argv}")
 
-html = sys.argv[1]
+branch_type = sys.argv[1]
+html = sys.argv[2]
+tag = sys.argv[3]
 system_manifest = {}
 
 with open('system.json', 'r') as file:
-     system_manifest = json.load(file)
+    system_manifest = json.load(file)
 
-download_html = html.replace("releases/tag/", "releases/download/")
-system_manifest["download"] = f"{download_html}/release.zip"
+if branch_type == "feature":
+    download_html = html.replace("releases/tag/", "releases/download/")
+    system_manifest["download"] = f"{download_html}/release.zip"
+
+system_manifest["version"] = tag[1:]
 
 with open("updated_system.json", "w") as outfile:
     outfile.write(json.dumps(system_manifest, indent = 4))
