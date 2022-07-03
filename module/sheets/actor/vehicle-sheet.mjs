7973c6233fc739df730f6948206dfe94fe0dffb5
Nathan Douglas <Nathan20467@gmail.com>
2022-07-03 17:17:56 +0100

    Added weapons to vehicles

--- module/sheets/actor/vehicle-sheet.mjs
+++ module/sheets/actor/vehicle-sheet.mjs
@@ -18,6 +18,7 @@ export class MetalSaviorsVehicleSheet extends ActorSheet {
 		context.vehicleDesignations = [{ name: "Air" }, { name: "Land" }, { name: "Water" }];
 		context.vehicleCombatReadiness = [{ name: "Combat" }, { name: "Non-Combat" }];
 		context.data = actorData.data;
+		context.flags = actorData.flags;
 
 		this._prepareItems(context);
 		this._prepareVehicleData(context);
@@ -31,6 +32,7 @@ export class MetalSaviorsVehicleSheet extends ActorSheet {
 		// Initialize containers.
 		const gear = [];
 		let combatTraining = null;
+		const weapons = [];
 
 		// Iterate through items, allocating to containers
 		for (let i of context.items) {
@@ -43,12 +45,16 @@ export class MetalSaviorsVehicleSheet extends ActorSheet {
 				case "combatTraining":
 					combatTraining = i;
 					break;
+				case "weapon":
+					weapons.push(i);
+					break;
 			}
 		}
 
 		// Assign and return
 		context.gear = gear;
 		context.combatTraining = combatTraining;
+		context.weapons = weapons;
 	}
 
 	_prepareVehicleData(context) {
@@ -58,5 +64,66 @@ export class MetalSaviorsVehicleSheet extends ActorSheet {
 		// The following are only included for tokens
 		context.curMovementSpeed = token.combatant?.getCurMovementSpeedKey();
 		context.excessMomentum = token.combatant?.getExtraMovementMomentum();
+	}
+
+	activateListeners(html) {
+		super.activateListeners(html);
+
+		// Render the item sheet for viewing/editing prior to the editable check.
+		html.find(".item-edit").click((ev) => {
+			const li = $(ev.currentTarget).parents(".item");
+			const item = this.actor.items.get(li.data("itemId"));
+			item.sheet.render(true);
+		});
+
+		if (!this.isEditable) return;
+
+		// Delete Inventory Item
+		html.find(".item-delete").click((ev) => {
+			const li = $(ev.currentTarget).closest(".item");
+			const item = this.actor.items.get(li.data("itemId"));
+			item.delete();
+			li.slideUp(200, () => this.render(false));
+		});
+
+		html.find(".rollable").click(this._onRoll.bind(this));
+
+		html.find(".cur-weapon-select").click(async (ev) => {
+			const li = $(ev.currentTarget).closest(".item");
+			const weaponId = li.data("itemId");
+			const weapon = this.actor.items.get(weaponId);
+			if (weapon === this.actor.getCurWeapon()) {
+				this.actor.setCurWeapon(null);
+			} else {
+				this.actor.setCurWeapon(weapon);
+			}
+		});
+	}
+
+	_onRoll(event) {
+		event.preventDefault();
+		const element = event.currentTarget;
+		const dataset = element.dataset;
+
+		// Handle item rolls.
+		if (dataset.rollType) {
+			if (dataset.rollType == "item") {
+				const itemId = element.closest(".item").dataset.itemId;
+				const item = this.actor.items.get(itemId);
+				if (item) return item.roll(event);
+			}
+		}
+
+		// Handle rolls that supply the formula directly.
+		if (dataset.roll) {
+			let label = dataset.label ? `[roll] ${dataset.label}` : "";
+			let roll = new Roll(dataset.roll, this.actor.getRollData());
+			roll.toMessage({
+				speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+				flavor: label,
+				rollMode: game.settings.get("core", "rollMode"),
+			});
+			return roll;
+		}
 	}
 }
