ab7e05c6c1b2706f50c11ba28bc8b685628b9499
Nathan Douglas <Nathan20467@gmail.com>
2022-09-10 12:51:22 +0100

    #patch fixed attack augment costs

--- module/types/Combat/AttackAugment.js
+++ module/types/Combat/AttackAugment.js
@@ -2,12 +2,12 @@ import { AttackAugmentType } from "./Enums.js";
 
 export class AttackAugment {
 	static None = new AttackAugment({ type: AttackAugmentType.None });
-	static AimDownSights = new AttackAugment({ type: AttackAugmentType.AimDownSights, additionalActions: 2 });
+	static AimDownSights = new AttackAugment({ type: AttackAugmentType.AimDownSights, additionalActions: 1 });
 	static AllInStrike = new AttackAugment({ type: AttackAugmentType.AllInStrike, additionalActions: 2 });
 	static CalledStrike = new AttackAugment({ type: AttackAugmentType.CalledStrike, additionalActions: 1 });
 	static Cleave = new AttackAugment({ type: AttackAugmentType.Cleave, additionalActions: 1 });
 	static Feint = new AttackAugment({ type: AttackAugmentType.Feint, additionalActions: 1 });
-	static PreciseStrike = new AttackAugment({ type: AttackAugmentType.PreciseStrike, additionalActions: 2 });
+	static PreciseStrike = new AttackAugment({ type: AttackAugmentType.PreciseStrike, additionalActions: 1 });
 	static Spray = new AttackAugment({ type: AttackAugmentType.Spray, additionalActions: 1 });
 	static Unspecified = new AttackAugment({ type: AttackAugmentType.Unspecified });
 
